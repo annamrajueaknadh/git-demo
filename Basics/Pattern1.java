18914b56812002d790cdced5a1215dfbcde40d20
annamrajueaknadh <eaknadh2002@gmail.com>
2023-11-10 08:52:55 +0530

    second commit

--- Basics/Pattern1.java
+++ Basics/Pattern1.java
@@ -4,7 +4,7 @@ public class Pattern1{
     public static void main(String args[])
     {
         Scanner s = new Scanner(System.in);
-        System.out.print("enter no.of Stars u want : ");
+        System.out.print("enter  done no.of Stars u want : ");
         int n=s.nextInt();
         int i,j;
         for(i=1;i<=n;i++){
